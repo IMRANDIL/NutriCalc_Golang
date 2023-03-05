537f2e3f231d81f0dae9227fb149527bea7f023e
IMRANDIL <aliimranadil2@gmail.com>
2023-03-05 14:08:07 +0530

    added some go code on mar5

---
+++ API/CMD/main.go
@@ -0,0 +1,17 @@
+package main
+
+
+
+
+func main() {
+ns := GetNutritionalScore(
+	NutritionalData{
+		Energy:,
+		Sugars:,
+		SaturatedFattyAcid:,
+		Sodium:,
+		Fruits:,
+		Fibre:,
+		Protein:,
+	},Food)
+}
